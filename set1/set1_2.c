7f2aec9eaf803045cea1029ee78800e55fa1d6b7
vanathid <36979402+vanathid@users.noreply.github.com>
2018-03-03 21:45:31 +0530

    Create set1_2.c

---
+++ set1/set1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+   // if(c=='a' ||c=='e'||c=='i'||c=='o'||c=='u')
+    if(num%2==0) printf("even");
+    else 
+    printf("odd");
+    return 0;
+    }

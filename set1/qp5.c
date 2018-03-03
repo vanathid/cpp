80bb0c194988210c3026728913bd4cd6c87cdf93
vanathid <36979402+vanathid@users.noreply.github.com>
2018-03-03 22:15:20 +0530

    Create qp5.c

---
+++ set1/qp5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {if(a>c) printf("a is greatest");
+    else printf("c is greatest");}
+    else
+    {if(b>c) printf("b is greatest");
+    else printf("c is greatest");}
+    return 0;
+    }

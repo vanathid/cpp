7037c84cf10821932a3c70532dae73333824ebf5
vanathid <36979402+vanathid@users.noreply.github.com>
2018-03-03 22:17:28 +0530

    Update qp5.c

--- set1/qp5.c
+++ set1/qp5.c
@@ -5,10 +5,10 @@ int main()
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
     if(a>b)
-    {if(a>c) printf("a is greatest");
-    else printf("c is greatest");}
+    {if(a>c) printf("%d",a);
+    else printf("%d",c);}
     else
-    {if(b>c) printf("b is greatest");
-    else printf("c is greatest");}
+    {if(b>c) printf("%d",b");
+    else printf("%d",c");}
     return 0;
     }

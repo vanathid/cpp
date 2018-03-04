8a4b07c2f7120e72e46a4922bfd61bbe661a58bd
vanathid <36979402+vanathid@users.noreply.github.com>
2018-03-04 07:00:56 +0530

    Update qp6.c

--- set1/qp6.c
+++ set1/qp6.c
@@ -4,7 +4,7 @@ int main()
 {
     int year;
     scanf("%d",&year);
-    if(((year%400==0)&&(year%100==0))||(year%4)==0) printf("leap year");
-    else printf("not a leap year");
+    if(((year%400==0)&&(year%100==0))||(year%4)==0) printf("yes");
+    else printf("no");
     return 0;
     }

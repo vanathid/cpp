771a6514c31b7abe5ad61c17b7f1aa432f7e8182
vanathid <36979402+vanathid@users.noreply.github.com>
2018-03-06 11:05:02 +0530

    Update qp15.c
    15 - Display even numbers between two intervals.

--- set2/qp15.c
+++ set2/qp15.c
@@ -4,9 +4,9 @@ int main()
 {
     int n,m,j;
     scanf("%d%d",&n,&m);
-    if(n%2==0) n=n;
-    else n=n+1;
-    for(j=n;j<=m;j=j+2)
+    if(n%2==0) n=n+2;
+    else n=n+3;
+    for(j=n;j<m;j=j+2)
     {
         printf("%d\t",j);
     }

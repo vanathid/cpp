d97a18014b503b945d74babd13d9af05b434ba15
vanathid <36979402+vanathid@users.noreply.github.com>
2018-03-06 11:02:48 +0530

    Update qp14.c

--- set2/qp14.c
+++ set2/qp14.c
@@ -4,8 +4,8 @@ int main()
 {
     int n,m,j;
     scanf("%d%d",&n,&m);
-    if(n%2==0) n=n;
-    else n=n+1;
+    if(n%2==0) n=n+2;
+    else n=n+3;
     for(j=n;j<=m;j=j+2)
     {
         printf("%d\t",j);

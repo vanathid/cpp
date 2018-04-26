891403ddfe8e045b622a1d7a4f59077ca8a38de7
vanathid <36979402+vanathid@users.noreply.github.com>
2018-04-26 12:36:30 +0530

    Update armstrong_interval.c

--- set2/armstrong_interval.c
+++ set2/armstrong_interval.c
@@ -1,18 +1,19 @@
 int main(void) {
-  int n,sum,n1,n2,m,i;
+  int n,sum,m1,m2,m,i;
   scanf("%d",&n);
   scanf("%d",&m);
   for(i=n;i<=m;i++)
   {sum=0;
-  n1=i;
+  m1=i;
   do
   {
-    n2=n1%10;
-    n1=n1/10;
-    sum=sum+pow(n2,3);
-      }while(n1>0);
+    m2=m1%10;
+    m1=m1/10;
+    sum=sum+pow(m2,3);
+      }while(m1>0);
   if(i==sum)
   printf("%d\t",i);
   }
   return 0;
 }
+

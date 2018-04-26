b29ba117b2aa4da0a10c8574890749730e14341e
vanathid <36979402+vanathid@users.noreply.github.com>
2018-04-26 12:19:42 +0530

    Update qp17.c

--- set2/qp17.c
+++ set2/qp17.c
@@ -1,1 +1,16 @@
-
+int main(void) {
+  int n,sum=0,n1,n2;
+  scanf("%d",&n);
+  n1=n;
+  do
+  {
+    n2=n1%10;
+    n1=n1/10;
+    sum=sum+pow(n2,3);
+      }while(n1>0);
+  if(n==sum)
+  printf("yes");
+  else
+  printf("no");
+  return 0;
+}
